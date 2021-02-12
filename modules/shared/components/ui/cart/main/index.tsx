dde163293b2c7476bce3c42262c6c7fdec148d39
Neznayka <bugayow.andrei2014@gmail.com>
2021-02-12 14:42:46 +0200

    ''

---
+++ modules/shared/components/ui/cart/main/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {MWrapper, MCWrapper} from './views'
+
+const Cart = () => {
+  return (
+      <MWrapper>
+        <MCWrapper>
+
+        </MCWrapper>
+      </MWrapper>
+  );
+};
+
+export default Cart;
---
+++ modules/shared/components/ui/cart/main/views.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const MWrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: rgb(0,0,0,0.4);
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const MCWrapper = styled.div`
+  background-color: white;
+  padding: 20px;
+  width: 200px;
+  height: 200px;
+  border-radius: 10px;
+`
